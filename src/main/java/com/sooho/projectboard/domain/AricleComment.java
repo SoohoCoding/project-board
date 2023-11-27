ac26010a2bb6c1e23f10a99c4b3c282e7da15ad0
soohocoding <ckdgns1447@gmail.com>
2023-11-27 13:21:47 +0900

    ERD 도메인설계 수정(2)

--- src/main/java/com/sooho/projectboard/domain/AricleComment.java
+++ src/main/java/com/sooho/projectboard/domain/AricleComment.java
@@ -5,11 +5,11 @@ import java.time.LocalDateTime;
 public class AricleComment {
 
     private Long id;
-    private Article article;
-    private String title;
+    private Article article; //게시글 (ID)
+    private String title; // 해시태그
 
-    private LocalDateTime createAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createAt; // 생서일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; //수정일시
+    private String modifiedBy; // 수정자
 }
