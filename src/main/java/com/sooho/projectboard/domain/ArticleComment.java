725116a130ba4e047f0c2ab336a6fae54846068d
soohocoding <ckdgns1447@gmail.com>
2023-12-05 16:27:24 +0900

    버전 변경으로인한 오류 수정

--- src/main/java/com/sooho/projectboard/domain/ArticleComment.java
+++ src/main/java/com/sooho/projectboard/domain/ArticleComment.java
@@ -1,10 +1,11 @@
 package com.sooho.projectboard.domain;
 
-import jakarta.persistence.*;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.util.Objects;
 
 
